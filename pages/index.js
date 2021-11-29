3edd1d689d12da220e9d141eb1677e8bd5ab7219
Rojae Martin <rojaedmartin@gmail.com>
2021-11-29 14:10:32 -0500

    making processes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,40 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [quantum, setQuantum] = useState(0);
+  const [noProcesses, setNoProcesses] = useState(4);
+  const [minLen, setMinLen] = useState(1);
+  const [maxLen, setMaxLen] = useState(1);
+  const [minTime, setMinTime] = useState(1);
+  const [maxTime, setMaxTime] = useState(1);
+  const [processes, setProcesses] = useState([]);
+
+  useEffect(() => {
+    console.log(processes);
+  }, [processes]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let processList = [];
+    setMaxLen(Math.floor(maxLen));
+    setMinLen(Math.ceil(minLen));
+    setMaxTime(Math.floor(maxTime));
+    setMinTime(Math.ceil(minTime));
+    setQuantum(Math.floor(quantum));
+
+    for (let x = 0; x < Math.floor(noProcesses); x++) {
+      processList.push({
+        processTime:
+          Math.floor(Math.random() * (maxTime - minTime + 1)) + minTime,
+        processLength:
+          Math.floor(Math.random() * (maxLen - minLen + 1)) + minLen,
+      });
+    }
+    setProcesses(processList);
+  }
+
   return (
     <div className="container">
       <Head>
@@ -9,43 +43,75 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className="1">
+          <form onSubmit={handleSubmit}>
+            <select name="algorithms" id="selector">
+              <option value="fcfs">First Come First Serve</option>
+              <option value="sjb">Shortest Job First</option>
+              <option value="ps">Priority Scheduling</option>
+              <option value="rr">Round Robin</option>
+            </select>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+            <label htmlFor="no_processes">Number of Processes</label>
+            <input
+              type="number"
+              value={noProcesses}
+              id="no_processes"
+              name="no_processes"
+              className="options"
+              onChange={(e) => setNoProcesses(e.target.value)}
+            ></input>
+            <label htmlFor="quantum">Quantum</label>
+            <input
+              type="number"
+              value={quantum}
+              id="quantum"
+              name="quantum"
+              className="options"
+              onChange={(e) => setQuantum(e.target.value)}
+            ></input>
+            <label htmlFor="min_length">Min Length</label>
+            <input
+              type="number"
+              value={minLen}
+              id="min_length"
+              name="min_length"
+              className="options"
+              onChange={(e) => setMinLen(e.target.value)}
+            ></input>
+            <label htmlFor="max_length">Max Length</label>
+            <input
+              type="number"
+              value={maxLen}
+              id="max_length"
+              name="max_length"
+              className="options"
+              onChange={(e) => setMaxLen(e.target.value)}
+            ></input>
+            <label htmlFor="min_time">Min Time</label>
+            <input
+              type="number"
+              value={minTime}
+              id="min_time"
+              name="min_time"
+              className="options"
+              onChange={(e) => setMinTime(e.target.value)}
+            ></input>
+            <label htmlFor="max_time">Max Time</label>
+            <input
+              type="number"
+              value={maxTime}
+              id="max_time"
+              name="max_time"
+              className="options"
+              onChange={(e) => setMaxTime(e.target.value)}
+            ></input>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <input type="submit" value="Generate Processes" />
+          </form>
         </div>
+
+        <div className="process_details"></div>
       </main>
 
       <footer>
@@ -54,8 +120,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +270,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
